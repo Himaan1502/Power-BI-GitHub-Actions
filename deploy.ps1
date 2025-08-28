2ab7ac6f5a384fce7fc0cca44ce57eded1b6027c
Himaanshu Sharma <sharmahimaanshu15028@gmail.com>
2025-08-28 13:13:12 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,8 @@
 param (
-    [Parameter(Mandatory=$true)][string]$WorkspaceId,
-    [Parameter(Mandatory=$true)][string]$TenantId,
-    [Parameter(Mandatory=$true)][string]$ClientId,
-    [Parameter(Mandatory=$true)][string]$ClientSecret
+    [Parameter(Mandatory = $true)][string]$WorkspaceId,
+    [Parameter(Mandatory = $true)][string]$TenantId,
+    [Parameter(Mandatory = $true)][string]$ClientId,
+    [Parameter(Mandatory = $true)][string]$ClientSecret
 )
 
 Write-Host "Authenticating to Power BI Service..."
